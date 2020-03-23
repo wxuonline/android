6dc3f4fc2fac44c914b4ed171bb1fb32239ea6bd
wxu <wxuonline@gmail.com>
2020-03-23 18:08:06 +0800

    20200323

--- app/src/main/java/com/example/myfirstapp/TestActivity.java
+++ app/src/main/java/com/example/myfirstapp/TestActivity.java
@@ -3,42 +3,32 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TestActivity extends AppCompatActivity {
-    private Button btn_down;
-    private Button btn_up;
-    private ScrollView scrollView;
+    private Chronometer chronometer;
+    private Button btn_start,btn_stop,btn_base,btn_format;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        bindViews();
+        String[] strs = {"基神","B神","翔神","曹神","J神"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, strs);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
     }
 
-    private void bindViews() {
-        btn_down = (Button) findViewById(R.id.btn_down);
-        btn_up = (Button) findViewById(R.id.btn_up);
-        scrollView = (ScrollView) findViewById(R.id.scrollView);
-//        btn_down.setOnClickListener((View.OnClickListener) this);
-//        btn_up.setOnClickListener((View.OnClickListener) this);
-    }
 
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_down:
-                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
-                break;
-            case R.id.btn_up:
-                scrollView.fullScroll(ScrollView.FOCUS_UP);
-                break;
-        }
-    }
 }
 
 
@@ -47,4 +37,4 @@ public class TestActivity extends AppCompatActivity {
 
 
 
-//2020-03-13 https://www.runoob.com/w3cnote/android-tutorial-edittext.html
+//2020-03-23 https://www.runoob.com/w3cnote/android-tutorial-adapter.html
