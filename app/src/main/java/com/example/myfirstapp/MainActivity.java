0b810f081e87e3926a8719d1fdc0bcd3e01e9b2a
wxu <wxuonline@gmail.com>
2020-03-25 18:05:11 +0800

    20200325

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions.RxPermissions;
+
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.mainstay.MESSAGE";
@@ -15,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
     /** Called when the user taps the Send button */
@@ -39,8 +50,26 @@ public class MainActivity extends AppCompatActivity {
 //        set.start();
     }
     public void openActivity(View view) {
-        Intent intent = new Intent(this, TestActivity.class);
-        startActivity(intent);
+        System.out.println("000");
+        RxPermissions.getInstance(this)
+                .request(Manifest.permission.CAMERA)//这里填写所需要的权限
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean aBoolean) {
+                        System.out.println("111");
+                        if (aBoolean) {//true表示获取权限成功（注意这里在android6.0以下默认为true）
+                            System.out.println("222");
+//                            Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+//                            System.out.println(cursor);
+                            Log.i("permissions", "Manifest.permission.CAMERA" + "：" + "获取成功");
+                        } else {
+                            System.out.println("333");
+                            Log.i("permissions", "Manifest.permission.CAMERA" + "：" + "获取失败");
+                        }
+                    }
+                });
+//        Intent intent = new Intent(this, TestActivity.class);
+//        startActivity(intent);
 //        Toast.makeText(getApplicationContext(),"Love",Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/myfirstapp/TestActivity.java
+++ app/src/main/java/com/example/myfirstapp/TestActivity.java
@@ -2,39 +2,55 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.tbruyelle.rxpermissions.RxPermissions;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import rx.functions.Action1;
 
 public class TestActivity extends AppCompatActivity {
-    private Chronometer chronometer;
-    private Button btn_start,btn_stop,btn_base,btn_format;
+    private String[] names = new String[]{"B神", "基神", "曹神"};
+    private String[] says = new String[]{"无形被黑，最为致命", "大神好厉害~", "我将带头日狗~"};
+    private int[] imgIds = new int[]{R.mipmap.tmall, R.mipmap.jd, R.mipmap.jn};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        List<String> data = new ArrayList<String>();
-        data.add("基神");
-        data.add("B神");
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,data);
-//        List<String> data = new ArrayList<String>();
-//        String[] strs = {"基神","B神","翔神","曹神","J神"};
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, strs);
+        List<Map<String, Object>> listitem = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < names.length; i++) {
+            Map<String, Object> showitem = new HashMap<String, Object>();
+            showitem.put("portrait", imgIds[i]);
+            showitem.put("name", names[i]);
+            showitem.put("says", says[i]);
+            listitem.add(showitem);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(this, listitem, R.layout.item, new String[]{"portrait", "name", "says"}, new int[]{R.id.imgtou, R.id.name, R.id.says});
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
     }
+
 
 
 }
