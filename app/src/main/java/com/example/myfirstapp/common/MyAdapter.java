8971c471ac99b076a1b8c0c818b2e6490e5f139a
wxu <wxuonline@gmail.com>
2020-03-30 18:09:02 +0800

    20200330

--- app/src/main/java/com/example/myfirstapp/common/MyAdapter.java
+++ app/src/main/java/com/example/myfirstapp/common/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myfirstapp.common;
 
 import android.content.Context;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,26 +9,31 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.myfirstapp.R;
+import java.util.ArrayList;
 
-import java.util.LinkedList;
 
-public class MyAdapter extends BaseAdapter {
-    private Context mContext;
-    private LinkedList<Data> mData;
+public abstract class MyAdapter<T> extends BaseAdapter {
 
-    public MyAdapter(Context mContext, LinkedList mData) {
-        this.mContext = mContext;
+    private ArrayList<T> mData;
+    private int mLayoutRes;           //布局id
+
+
+    public MyAdapter() {
+    }
+
+    public MyAdapter(ArrayList<T> mData, int mLayoutRes) {
         this.mData = mData;
+        this.mLayoutRes = mLayoutRes;
     }
 
     @Override
     public int getCount() {
-        return mData.size();
+        return mData != null ? mData.size() : 0;
     }
+
     @Override
-    public Object getItem(int position) {
-        return null;
+    public T getItem(int position) {
+        return mData.get(position);
     }
 
     @Override
@@ -37,31 +43,160 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder = null;
-        if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.item, parent, false);
-            holder = new ViewHolder();
-            holder.img_icon = (ImageView) convertView.findViewById(R.id.img_icon);
-            holder.txt_content = (TextView) convertView.findViewById(R.id.txt_content);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-        holder.img_icon.setImageResource(mData.get(position).getImgId());
-        holder.txt_content.setText(mData.get(position).getContent());
-        return convertView;
+        ViewHolder holder = ViewHolder.bind(parent.getContext(), convertView, parent, mLayoutRes
+                , position);
+        bindView(holder, getItem(position));
+        return holder.getItemView();
     }
 
-    private class ViewHolder{
-        ImageView img_icon;
-        TextView txt_content;
-    }
+    public abstract void bindView(ViewHolder holder, T obj);
 
-    public void add(Data data) {
+    //添加一个元素
+    public void add(T data) {
         if (mData == null) {
-            mData = new LinkedList<>();
+            mData = new ArrayList<>();
         }
         mData.add(data);
         notifyDataSetChanged();
     }
+
+    //往特定位置，添加一个元素
+    public void add(int position, T data) {
+        if (mData == null) {
+            mData = new ArrayList<>();
+        }
+        mData.add(position, data);
+        notifyDataSetChanged();
+    }
+
+    public void remove(T data) {
+        if (mData != null) {
+            mData.remove(data);
+        }
+        notifyDataSetChanged();
+    }
+
+    public void remove(int position) {
+        if (mData != null) {
+            mData.remove(position);
+        }
+        notifyDataSetChanged();
+    }
+
+    public void clear() {
+        if (mData != null) {
+            mData.clear();
+        }
+        notifyDataSetChanged();
+    }
+
+
+    public static class ViewHolder {
+
+        private SparseArray<View> mViews;   //存储ListView 的 item中的View
+        private View item;                  //存放convertView
+        private int position;               //游标
+        private Context context;            //Context上下文
+
+        //构造方法，完成相关初始化
+        private ViewHolder(Context context, ViewGroup parent, int layoutRes) {
+            mViews = new SparseArray<>();
+            this.context = context;
+            View convertView = LayoutInflater.from(context).inflate(layoutRes, parent, false);
+            convertView.setTag(this);
+            item = convertView;
+        }
+
+        //绑定ViewHolder与item
+        public static ViewHolder bind(Context context, View convertView, ViewGroup parent,
+                                      int layoutRes, int position) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder(context, parent, layoutRes);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+                holder.item = convertView;
+            }
+            holder.position = position;
+            return holder;
+        }
+
+        @SuppressWarnings("unchecked")
+        public <T extends View> T getView(int id) {
+            T t = (T) mViews.get(id);
+            if (t == null) {
+                t = (T) item.findViewById(id);
+                mViews.put(id, t);
+            }
+            return t;
+        }
+
+
+        /**
+         * 获取当前条目
+         */
+        public View getItemView() {
+            return item;
+        }
+
+        /**
+         * 获取条目位置
+         */
+        public int getItemPosition() {
+            return position;
+        }
+
+        /**
+         * 设置文字
+         */
+        public ViewHolder setText(int id, CharSequence text) {
+            View view = getView(id);
+            if (view instanceof TextView) {
+                ((TextView) view).setText(text);
+            }
+            return this;
+        }
+
+        /**
+         * 设置图片
+         */
+        public ViewHolder setImageResource(int id, int drawableRes) {
+            View view = getView(id);
+            if (view instanceof ImageView) {
+                ((ImageView) view).setImageResource(drawableRes);
+            } else {
+                view.setBackgroundResource(drawableRes);
+            }
+            return this;
+        }
+
+
+        /**
+         * 设置点击监听
+         */
+        public ViewHolder setOnClickListener(int id, View.OnClickListener listener) {
+            getView(id).setOnClickListener(listener);
+            return this;
+        }
+
+        /**
+         * 设置可见
+         */
+        public ViewHolder setVisibility(int id, int visible) {
+            getView(id).setVisibility(visible);
+            return this;
+        }
+
+        /**
+         * 设置标签
+         */
+        public ViewHolder setTag(int id, Object obj) {
+            getView(id).setTag(obj);
+            return this;
+        }
+
+        //其他方法可自行扩展
+
+    }
+
 }
