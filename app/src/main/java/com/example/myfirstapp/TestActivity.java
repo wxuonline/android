1e1f939ab8e04b596cbedcdd01f634c628ca54a6
wxu <wxuonline@gmail.com>
2020-04-27 18:15:28 +0800

    20200427

--- app/src/main/java/com/example/myfirstapp/TestActivity.java
+++ app/src/main/java/com/example/myfirstapp/TestActivity.java
@@ -9,12 +9,15 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.database.Cursor;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.myfirstapp.broadcastReceiver.MyBcReceiver;
@@ -32,89 +35,46 @@ public class TestActivity extends AppCompatActivity implements View.OnClickListe
 //    private LocalBroadcastManager localBroadcastManager;
 //    private IntentFilter intentFilter;
     private String TAG = "test";
+    private ImageView imgView;
+    private AnimationDrawable ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        getData(new CallBack() {
+        imgView = (ImageView) findViewById(R.id.loading);
+        ad = (AnimationDrawable) imgView.getDrawable();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void getResult(String result) {
-                Log.d(TAG, "getResult: " + result);
+            public void run() {
+                ad.start();
             }
-        });
-
-//        TestFragment tf = new TestFragment();
-//        getSupportFragmentManager().beginTransaction().replace(R.id.frameContain, tf).commit();
-//        getSupportFragmentManager().beginTransaction().show(tf);
+        }, 300);
 
 
 
-//        RxPermissions.getInstance(TestActivity.this)
-//            .request(Manifest.permission.READ_SMS)
-//            .subscribe(new Action1<Boolean>() {
+
+//        getData(new CallBack() {
 //            @Override
-//            public void call(Boolean aBoolean) {
-//                if (aBoolean) {//true表示获取权限成功（在android6.0以下默认为true）
-//                    Log.d(TAG, "Manifest.permission.READ_SMS 获取成功");
-//                    getMsgs(TestActivity.this);
-//                } else {
-//                    Log.d(TAG, "Manifest.permission.READ_SMS 获取失败");
-//                }
+//            public void getResult(String result) {
+//                Log.d(TAG, "getResult: " + result);
 //            }
 //        });
-////        localBroadcastManager = LocalBroadcastManager.getInstance(this);
-////        localReceiver = new MyBcReceiver();
-////        intentFilter = new IntentFilter();
-////        intentFilter.addAction("com.jay.mybcreceiver.LOGIN_OTHER");
-////        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
-////
-//        Button btn = (Button) findViewById(R.id.btn);
-//        btn.setOnClickListener(this);
     }
 
-    public interface CallBack {
-        public void getResult(String result);
-    }
-
-    public void getData(CallBack callBack) {
-        String msg = TestActivity.this.getPackageName();
-        callBack.getResult(msg);
-    }
-
-//    public void getMsgs(Context context) {
-//        Uri uri = Uri.parse("content://sms/");
-//        Cursor cursor = getContentResolver().query(uri, new String[]{"address", "date", "type", "body"}, null, null, null);
-//        while (cursor.moveToNext()) {
-//            String address = cursor.getString(0);
-//            String date = cursor.getString(1);
-//            String type = cursor.getString(2);
-//            String body = cursor.getString(3);
-//            Log.d(TAG, "getMsgs: address => " + address);
-//            Log.d(TAG, "getMsgs: date => " + date);
-//            Log.d(TAG, "getMsgs: type => " + type);
-//            Log.d(TAG, "getMsgs: body => " + body);
-//        }
-//        cursor.close();
+//    public interface CallBack {
+//        public void getResult(String result);
+//    }
+//
+//    public void getData(CallBack callBack) {
+//        String msg = TestActivity.this.getPackageName();
+//        callBack.getResult(msg);
 //    }
 
     @Override
     public void onClick(View view) {
-//        Intent intent = new Intent("com.jay.mybcreceiver.LOGIN_OTHER");
-//        localBroadcastManager.sendBroadcast(intent);
-//        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-//        intent.addCategory(Intent.CATEGORY_OPENABLE);
-//        intent.setType("image/*");
-//        startActivity(intent);
-        Intent it = new Intent();
-//        it.setAction(Intent.ACTION_MAIN);
-//        it.addCategory(Intent.CATEGORY_HOME);
-//        it.setAction(Intent.ACTION_VIEW);
-//        it.setData(Uri.parse("http://www.baidu.com"));
-        it.setAction("my_action");
-        it.addCategory("my_category");
-        startActivity(it);
     }
 }
 
